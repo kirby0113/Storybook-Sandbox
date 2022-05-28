3ecfcde0cbfe138970e39998182084336b0fd2da
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-28 22:28:28 +0900

    feat:Slideボタンのアニメーションを作成

--- components/Buttons/SlideButton.tsx
+++ components/Buttons/SlideButton.tsx
@@ -10,13 +10,40 @@ export type SlideButtonProps = {
 const StyledSlideButton = styled.button<{ color: keyof typeof colors }>`
   font-size:25px;
   padding:10px 20px;
+  position:relative;
   font-weight:bold;
   letter-spacing:3px;
   color ${colors['white'].light};
   background-color: ${({ color }) => colors[color].normal};
-  border: solid 2px ${({ color }) => colors[color].dark};
+  border: solid 1px ${({ color }) => colors[color].normal};
   cursor:pointer;
   text-transform: uppercase;
+  transition:all .3s;
+  overflow:hidden;
+  z-index:1;
+  animation-direction: reverse;
+
+  &::before{
+    content:"";
+    width:110%;
+    height:110%;
+    z-index:-1;
+    position:absolute;
+    top:0%;
+    left:-110%;
+    background-color ${colors['white'].light};
+    transition:all .3s;
+  }
+
+  &:hover{
+    color: ${({ color }) => colors[color].normal};
+
+    &::before{
+      top:0;
+      left:0;
+      background-color ${colors['white'].light};
+    }
+  }
 `;
 
 export const SlideButton = ({ children, color = 'primary', onClick }: SlideButtonProps) => {
